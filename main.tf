c2e4ee3bd1f19832459a86ddd69fc5f8de1a6523
Adam Lewandowski <adres_smieciowy@outlook.com>
2023-05-08 10:24:52 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66.1"
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+    sid     = ""
+  }
+}
+
+data "archive_file" "myzip" {
+  type        = "zip"
+  source_file = "main.py"
+  output_path = "main.zip"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_lambda_function" "mypython_lambda" {
+  filename      = "main.zip"
+  function_name = "mypython_lambda_test"
+  role          = aws_iam_role.mypython_lambda_role.arn
+  handler       = "lambda_handler"
+  runtime       = "python3.10"
+}
+
+resource "aws_iam_role" "mypython_lambda_role" {
+  name               = "mypython_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
